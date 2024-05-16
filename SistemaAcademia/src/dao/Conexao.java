9f32b5a5a076213fa55f344d7dadde2cda4426c3
Henrique Sciani <135352184+HenriqueSciani@users.noreply.github.com>
2024-05-15 21:36:40 -0300

    Update Conexao java

--- SistemaAcademia/src/dao/Conexao.java
+++ SistemaAcademia/src/dao/Conexao.java
@@ -1,17 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 /**
  *
  * @author aluno
  */
 public class Conexao {
+
     private static final String DRIVER = "MySQL Connector/J";
     private static final String URL = "jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL";
     private static final String USER = "root";
     private static final String PASS = "1234";
-    
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(DRIVER);
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+
+    public static void closeConnection(Connection c) throws SQLException {
+        if (c != null && !c.isClosed()) {
+            c.close();
+        }
+    }
+
+    public static void closeConnection(Connection c, PreparedStatement statment) throws SQLException {
+        closeConnection(c);
+        if (statment != null) {
+            statment.close();
+        }
+    }
 }
